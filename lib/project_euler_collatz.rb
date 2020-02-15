f5ad6e1a84bb28113c1b765c33c106c4c53e26b2
Matt Bechtel <bechma02@gmail.com>
2020-02-15 18:29:28 +0000

    Automatically backed up by Learn

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,13 +1,28 @@
 def even_next(n)
+  n / 2
 end
 
 def odd_next(n)
+  3 * n + 1
 end
 
 def next_value(n)
+  if n % 2 == 0
+    result = even_next(n)
+  elsif
+    result = odd_next(n)
+  end
+  result
 end
 
 def collatz(n)
+  result = [n]
+  while n !== 1 do
+    n = next_value(n)
+    result << n
+  end
+
+  result
 end
 
 def longest_collatz
